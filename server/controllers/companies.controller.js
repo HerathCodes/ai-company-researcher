b85159a7e5dba1253ef502bdcde20d2d2786d815
Tharusha Herath <tharusha.herath13@gmail.com>
2024-04-03 19:07:56 -0400

    Added controllers folder and finished routing for companies in controllers.

---
+++ server/controllers/companies.controller.js
@@ -0,0 +1,64 @@
+const Company = require('../models/company.model');
+const mongoose = require('mongoose')
+
+
+
+//GET all companies
+const getCompanies = async(req,res) =>{
+    const companies = await Company.find({}).sort({createdAt: -1});
+    res.status(200).json(companies);
+}
+
+//GET a single company
+const getCompany = async(req,res) =>{
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: "No such company"});
+    }
+
+    const company = await Company.findById(id);
+
+    if(!company){
+        return res.status(400).json({error: "No such company"});
+    }
+    res.status(200).json(company);
+}
+
+
+
+// POST a new company
+const createCompany = async(req, res) => {
+    const {Name, Office, Summary, Positions, Salary, Links} = req.body;
+
+    try{
+        const company = await Company.create({Name, Office, Summary, Positions, Salary, Links});
+        res.status(200).json(company);
+    }catch (err){
+        res.status(404).json({error: err.message});
+    }
+}
+
+
+// DELETE a new company
+const deleteCompany = async(req, res) =>{
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: "No such company"});
+    }
+
+    const company = await Company.findOneAndDelete({_id: id});
+
+    if(!company){
+        return res.status(400).json({error: "No such company"});
+    }
+    res.status(200).json(company);
+}
+
+module.exports = {
+    getCompanies,
+    getCompany,
+    createCompany,
+    deleteCompany
+}
--- server/routes/companies.js
+++ server/routes/companies.js
@@ -1,34 +1,24 @@
 const express = require('express');
-const Company = require('../models/company.model');
+const {
+    getCompanies,
+    getCompany,
+    createCompany,
+    deleteCompany
+} = require('../controllers/companies.controller')
 
 const router  = express.Router()
 
 //GET all companies
-router.get('/', (req, res) => {
-    res.json({msg: "Get all companies"});
-});
+router.get('/', getCompanies);
 
 //GET single company
-router.get('/:id', (req,res) =>{
-    res.json({msg: "Get single company" + req.body});
-});
+router.get('/:id', getCompany);
 
 // POST a new company
-router.post('/', async (req, res) =>{
-    const {Name, Office, Summary, Positions, Salary, Links} = req.body;
-
-    try{
-        const company = await Company.create({Name, Office, Summary, Positions, Salary, Links});
-        res.status(200).json(company);
-    }catch (err){
-        res.status(404).json({error: err.message});
-    }
-});
+router.post('/', createCompany);
 
 // DELETE a new company
-router.delete('/:id', (req, res) =>{
-    res.json({msg: "DELETE single company"});
-});
+router.delete('/:id', deleteCompany);
 
 
 
