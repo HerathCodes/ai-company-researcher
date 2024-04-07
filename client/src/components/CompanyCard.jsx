489af8768562186a2a871dc269b07b87317dd1a1
methirah <methira.19@gmail.com>
2024-04-07 14:04:19 -0400

    added some search event handling

---
+++ client/src/components/CompanyCard.jsx
@@ -0,0 +1,13 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+
+import React from 'react'
+
+function CompanyCard() {
+  return (
+    <div>CompanyCard</div>
+  )
+}
+
+export default CompanyCard
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,8 +1,52 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCaretDown, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function Search(){
+
+    const [query, setQuery] = useState('');
+
+    async function suggestCompanies(e) {
+        e.preventDefault();
+        setQuery(e.target.value);
+        try {
+            const response = await fetch(`http://localhost:3000/api/companies/search/${query}`);
+
+            if (response.status === 500) {
+                console.error("Error fetching data");
+            } else if (response.status === 400) {
+                // no such company, do ur thingy
+            } else {
+                const data = await response.json();
+                console.log(data);
+                // iterate through data, display suggestions
+            }
+        } catch (error) {
+            console.error('GET Error: ' + error);
+        }
+    }
+
+    async function searchCompany(e) {
+        e.preventDefault();
+        try {
+            const response = await fetch(`http://localhost:3000/api/companies/${query}`);
+
+            if (response.status === 500) {
+                console.error("Error fetching data");
+            } else if (response.status === 400) {
+                // no such company, do ur thingy
+                
+            } else {
+                const data = await response.json();
+                console.log(data);
+                // display company
+            }
+        } catch (error) {
+            console.error('GET Error: ' + error);
+    }
+
+        
+    }
     return (
         <div className="search-container">
             <div className="dropDown">
@@ -18,8 +62,16 @@ function Search(){
             </div>
             
             <div className="search-box">
-                <input type="text" id="search-input" placeholder="search company..."></input>
-                <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                <form action={searchCompany} >
+                    <input 
+                        value={query}
+                        onChange={suggestCompanies}
+                        type="text" 
+                        placeholder="search company..."
+                        
+                    />
+                    <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                </form>
             </div>
         </div>
     )
