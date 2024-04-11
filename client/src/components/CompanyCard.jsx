4d12e87b7be96061ca3b53fda621a9f7f76c4fd9
methirah <methira.19@gmail.com>
2024-04-10 22:07:34 -0400

    card changes

--- client/src/components/CompanyCard.jsx
+++ client/src/components/CompanyCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
@@ -8,32 +8,35 @@ import Typography from '@mui/material/Typography';
 function CompanyCard(props) {
   const { query } = props;
 
-  if (query._id) {
-    return (
-      <Card>
-        <CardHeader
-          title={query.Name}
-          subheader={query.Office}
-        />
-        <CardContent>
-          <Typography variant="p" component="div">
-            {query.Summary}
-          </Typography>
-        </CardContent>
-      </Card>
-    )
-  } else {
-    // http://serpapi.com/playground?engine=google_about_this_result&api_key=${process.env.SERP_API_KEY}google_domain=google.com&q=${query.Name}
-    return (
-      <Card >
-        <CardContent>
-          <Typography variant="h5" component="div">
-              {query.Name}
-          </Typography>
-        </CardContent>
-      </Card>
-    )
-  }
+  return (
+    <Card>
+      <CardHeader
+        title={query.Name}
+        subheader={query.Office}
+      />
+      <CardContent>
+        <Typography variant="p" component="div">
+          {query.Summary}
+        </Typography>
+        {query.Links && query.Links.length > 0 && (
+          <div>
+            <Typography variant="h6" component="div">
+              Links:
+            </Typography>
+            <ul>
+              {query.Links.map((link, index) => (
+                <li key={index}>
+                  <a href={link.url} target="_blank" rel="noopener noreferrer">
+                    {link.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </CardContent>
+    </Card>
+  );
 }
 
-export default CompanyCard
+export default CompanyCard;
